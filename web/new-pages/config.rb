fb2a7a14b564d203b316c9844a799834adc03d55
Gourry <gourrymk2@gmail.com>
2012-09-13 02:03:48 +0800

    added new-pages

---
+++ web/new-pages/config.rb
@@ -0,0 +1,15 @@
+# Require any additional compass plugins here.
+project_type = :stand_alone
+
+# Publishing paths
+http_path = "/2012/"
+http_images_path = "/2012/images"
+http_fonts_path = "/2012/fonts"
+css_dir = "stylesheets"
+
+# Local development paths
+sass_dir = "sass"
+images_dir = "images"
+fonts_dir = "fonts"
+
+#output_style = :compressed
