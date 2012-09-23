21778b233c7701cb780f8a21bdb44ddea741b322
Gourry <gourrymk2@gmail.com>
2012-09-23 22:14:26 +0800

    adjusted google maps

--- 2012/javascripts/plugins.js
+++ 2012/javascripts/plugins.js
@@ -12,3 +12,46 @@ if (!(window.console && console.log)) {
 }
 
 // Place any jQuery/helper plugins in here.
+
+$(document).ready(function () {
+
+    /**
+     * adjust google map to a suitable size
+     */
+    function changeGoogleMapIframeSrc() {
+        var $map = $('#map');
+        if ($map.length > 0) {
+            $map.remove();
+        }
+
+        var bodyWidth = $('#main').css('width');
+        var html = '<iframe id="map" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="{{ src }}"></iframe>';
+        var src = '';
+
+        switch (parseInt(bodyWidth)) {
+            case 320:
+                src = 'https://maps.google.com.tw/maps/ms?msid=204336690330534160949.0004c9390224242bdb896&amp;msa=0&amp;ie=UTF8&amp;t=m&amp;brcurrent=3,0x3442ac6b61dbbd9d:0xc0c243da98cba64b,1&amp;source=embed&amp;ll=25.046103,121.612129&amp;spn=0.099533,0.085144&amp;z=14&amp;output=embed';
+                break;
+            case 480:
+                src = 'https://maps.google.com.tw/maps/ms?msid=204336690330534160949.0004c9390224242bdb896&amp;msa=0&amp;ie=UTF8&amp;t=m&amp;brcurrent=3,0x3442ac6b61dbbd9d:0xc0c243da98cba64b,1&amp;source=embed&amp;ll=25.04968,121.613159&amp;spn=0.037324,0.068321&amp;z=13&amp;output=embed';
+                break;
+            case 720:
+                src = 'https://maps.google.com.tw/maps/ms?msid=204336690330534160949.0004c9390224242bdb896&amp;msa=0&amp;ie=UTF8&amp;t=m&amp;brcurrent=3,0x3442ac6b61dbbd9d:0xc0c243da98cba64b,1&amp;source=embed&amp;ll=25.04968,121.613159&amp;spn=0.037324,0.068321&amp;z=14&amp;output=embed';
+                break;
+            case 960:
+                src = 'https://maps.google.com.tw/maps/ms?msid=204336690330534160949.0004c9390224242bdb896&amp;msa=0&amp;ie=UTF8&amp;t=m&amp;brcurrent=3,0x3442ac6b61dbbd9d:0xc0c243da98cba64b,1&amp;source=embed&amp;ll=25.04968,121.613159&amp;spn=0.037324,0.068321&amp;z=15&amp;output=embed';
+                break;
+            default:
+                return;
+        }
+
+        html = html.replace('{{ src }}', src);
+        $('.google-map-link a').attr('href', src);
+        $('.google-map').prepend(html);
+    }
+
+    changeGoogleMapIframeSrc();
+    $(window).resize(function () {
+        changeGoogleMapIframeSrc();
+    });
+});
