8959dbb6ec378404ea57caa45be17fcb61d81689
Gourry <gourrymk2@gmail.com>
2012-09-29 11:09:59 +0800

    added button to toggle archive news

--- 2012/javascripts/main.js
+++ 2012/javascripts/main.js
@@ -1,6 +1,27 @@
 $(function() {
-    $('a.indexMore').click(function() {
-        $('article.newsArchive').show();
+    $('#show-archive-news').click(function() {
+        var $hideArchiveNews = $('#hide-archive-news');
+
+        $('article.news-archive').show();
+
+        if ($hideArchiveNews.hasClass('hidden')) {
+            $hideArchiveNews.removeClass('hidden');
+        }
+        $(this).addClass('hidden');
+
+        return false;
+    });
+
+    $('#hide-archive-news').click(function() {
+        var $showArchiveNews = $('#show-archive-news');
+
+        $('article.news-archive').hide();
+
+        if ($showArchiveNews.hasClass('hidden')) {
+            $showArchiveNews.removeClass('hidden');
+        }
+        $(this).addClass('hidden');
+
         return false;
     });
 });
